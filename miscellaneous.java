060b7e6646c791880110b9c5ddc628a2f03342b0
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-06-22 18:58:52 +0530

    Update miscellaneous.java

--- miscellaneous.java
+++ miscellaneous.java
@@ -229,3 +229,22 @@ public class Solution {
         return size;
     }
 }
+
+//Median Finder
+
+class MedianFinder {
+   int i=0;
+    Queue q[]={new PriorityQueue<Integer>(Collections.reverseOrder()),new PriorityQueue<Integer>()};
+    
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        q[i].add(num);
+        q[i^=1].add(q[i^1].poll());
+    }
+
+    // Returnsa the median of current data stream
+    public double findMedian() {
+        return ((int)q[1].peek() + (int)q[i].peek() ) / 2.0;
+    }
+};
+
