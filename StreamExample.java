4bab94d2e8a35c47a790f77a45364e083f3d9fab
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-06-26 20:09:16 +0530

    Update StreamExample.java

--- StreamExample.java
+++ StreamExample.java
@@ -17,6 +17,10 @@ public class StreamExample
 		Stream<Integer> intStream = Stream.of(1,2,3,4);
 		List<Integer> intList = intStream.collect(Collectors.toList());
 		System.out.println(intList); //prints [1, 2, 3, 4]
+		
+		intStream = Stream.of(1,2,3,4); //stream is closed, so we need to create it again
+		Map<Integer,Integer> intMap = intStream.collect(Collectors.toMap(i -> i, i -> i+10));
+		System.out.println(intMap); //prints {1=11, 2=12, 3=13, 4=14}
 
 		
 		
