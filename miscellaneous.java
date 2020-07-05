61d994b851d4863d009f286939ecaed56ad8aaa8
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-07-05 13:15:53 +0530

    Update miscellaneous.java

--- miscellaneous.java
+++ miscellaneous.java
@@ -329,4 +329,32 @@ public class Main
 	}
 }
 
+//Sort by keys
+
+import java.util.*; 
+class Main { 
+
+	static Map<String, Integer> map = new HashMap<>(); 
+
+	public static void sortbykey() 
+	{ 
+		TreeMap<String, Integer> sorted = new TreeMap<>(map); 
+        LinkedHashMap<String, Integer> map1 = new LinkedHashMap<>(sorted);
+		 
+		for (Map.Entry<String, Integer> entry : map1.entrySet()) 
+			System.out.println("Key = " + entry.getKey() + 
+						", Value = " + entry.getValue());		 
+	} 
+	
+	public static void main(String args[]) 
+	{ 
+		map.put("Jayant", 80); 
+		map.put("Abhishek", 90); 
+		map.put("Anushka", 80); 
+		map.put("Amit", 75); 
+		map.put("Danish", 40); 
+		sortbykey(); 
+	} 
+} 
+
 
