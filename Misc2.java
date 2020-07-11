edc6115f7a12c862b2788a7760d3bad75e4c8101
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-07-11 11:47:53 +0530

    Update Misc2.java

--- Misc2.java
+++ Misc2.java
@@ -1572,3 +1572,93 @@ class Solution {
         }
     }
 }
+
+
+
+static void rotateMatrix( 
+        int N, int mat[][]) 
+    { 
+        // Consider all squares one by one 
+        for (int x = 0; x < N / 2; x++) { 
+            // Consider elements in group 
+            // of 4 in current square 
+            for (int y = x; y < N - x - 1; y++) { 
+                // Store current cell in 
+                // temp variable 
+                int temp = mat[x][y]; 
+  
+                // Move values from right to top 
+                mat[x][y] = mat[y][N - 1 - x]; 
+  
+                // Move values from bottom to right 
+                mat[y][N - 1 - x] 
+                    = mat[N - 1 - x][N - 1 - y]; 
+  
+                // Move values from left to bottom 
+                mat[N - 1 - x][N - 1 - y] = mat[N - 1 - y][x]; 
+  
+                // Assign temp to left 
+                mat[N - 1 - y][x] = temp; 
+            } 
+        } 
+    }
+
+
+
+
+
+public class Solution {
+    public int maxCoins(int[] nums) {
+        // Extend list with head and tail (both are 1), index starts at 1
+        int array[] = new int[nums.length + 2];
+        array[0] = 1;
+        array[array.length-1] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            array[i+1] = nums[i];
+        }
+
+        // Initialize DP arrays, 1 index based
+        int dp[][] = new int[array.length][array.length]; //dp[i][j] stands for max coins at i step, burst j
+        for (int i =0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                dp[i][j] = 0;
+            }
+        }
+
+        for (int i=1; i< array.length-1; i++) {
+            for (int j=i; j >=1; j--) {
+                // k as last
+                for (int k=j; k <= i; k++) {
+                    dp[j][i] = Math.max(array[j-1]*array[k]*array[i+1] + dp[j][k-1] + dp[k+1][i], dp[j][i]);
+                }
+            }
+        }
+
+        return dp[1][array.length-2];
+    }
+}
+
+
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.Map.Entry;
+
+public class Main
+{
+	public static void main(String[] args) {
+		String str="aaannkkiitttt";
+		Map<Character,Integer> hmap=new HashMap<Character,Integer>();
+		for(char a:str.toCharArray())
+		{
+		    hmap.merge(a,1,Integer::sum);
+		}
+		LinkedHashMap<Character, Integer> sortedMap = 
+        hmap.entrySet().stream().
+        sorted(Entry.comparingByValue(Collections.reverseOrder())).
+        collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+                             (e1, e2) -> e1, LinkedHashMap::new));
+		System.out.println(sortedMap);
+		
+	}
+}
