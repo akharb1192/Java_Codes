ace3c0564328de186ebf4af8fa4b4fc10339fa73
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-07-12 14:08:59 +0530

    Update Misc2.java

--- Misc2.java
+++ Misc2.java
@@ -1724,3 +1724,37 @@ public class Main {
     postOrder(ans);
 }  
 } 
+
+import java.util.*; 
+
+class GFG { 
+	public static int findMinRec(int arr[], int i, 
+								int sumCalculated, 
+								int sumTotal) 
+	{ 
+		if (i == 0) 
+			return Math.abs((sumTotal-sumCalculated) - 
+								sumCalculated); 
+		return Math.min(findMinRec(arr, i - 1, sumCalculated 
+								+ arr[i-1], sumTotal), 
+								findMinRec(arr, i-1, 
+								sumCalculated, sumTotal)); 
+	} 
+	public static int findMin(int arr[], int n) 
+	{ 
+		int sumTotal = 0; 
+		for (int i = 0; i < n; i++) 
+			sumTotal += arr[i]; 
+		return findMinRec(arr, n, 0, sumTotal); 
+	} 
+	
+	/* Driver program to test above function */
+	public static void main(String[] args) 
+	{ 
+		int arr[] = {3, 1, 4, 2, 2, 1}; 
+		int n = arr.length; 
+		System.out.print("The minimum difference"+ 
+						" between two sets is " + 
+						findMin(arr, n)); 
+	} 
+} 
